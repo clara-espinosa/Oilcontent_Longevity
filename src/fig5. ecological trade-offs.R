5105f14afa00594e9217ad67ff0a6f41fe451b64
Clara Espinosa del Alba <espinosaclara@uniovi.es>
2025-02-07 14:06:57 +0100

    figure 5 updated

--- src/fig5. ecological trade-offs.R
+++ src/fig5. ecological trade-offs.R
@@ -1,153 +1,217 @@
 # figure ecological tradeoffs
-library(tidyverse);library(readxl);library(rstatix)
+library(tidyverse);library(readxl);library(rstatix); library(patchwork)
 library(ggrepel):library(vegan);library(ggpubr);library (viridis)
 
-read.csv("data/species_traits.csv")-> summary_table
-summary_table%>%
-  get_summary_stats(Snw)
+col_order <- c( "#8E5005","darkgoldenrod3", "gold1", "orange","darkorange2", "orangered1" ,
+                "#08f9dd", "#21a8be", "#2d7faf", "#275381", "#42346f",
+                "#78E208")
 
 ## PANEL A) OIL content and ratio vs GDD ####
-sig.label <- as.data.frame(cbind(trait = c("Oil content (%)", "Ratio UFA/SFA"),
-                                 x= c(2100, 2100), y= c(30, 18), 
-                                 label = c("pMCMC: 0.32", "pMCMC: 0.54")))
-sig.label%>%
-  mutate(x= as.numeric(x),
-         y= as.numeric(y))->sig.label
-str(sig.label)
-
+# oil content
 read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
   mutate(family = as.factor(family))%>%
   mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
                               "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
                               "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
                               "Crassulaceae", "Saxifragaceae", "Poaceae", 
                               "Cyperaceae", "Juncaceae"))%>%
-  dplyr::select(community, Taxon, family, GDD, oil.content, ratio)%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
   na.omit()%>%
-  group_by(community, Taxon, family, GDD)%>%
-  gather(trait, value, oil.content:ratio)%>%
-  mutate(trait = as.factor(trait))%>%
-  mutate(trait = recode(trait, "oil.content"= "Oil content (%)", "ratio"= "Ratio UFA/SFA"))%>%
-  #data.frame()
-  ggplot(aes(y=value, x = GDD))+ #, fill = family
-  geom_point(aes(fill = family),shape = 21, color = "black", show.legend = F, size= 4)+
-  scale_fill_manual (values=col)+
+  ggplot(aes(y=oil.content, x = GDD))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = F, size= 4)+
+  scale_fill_manual (values=col_order)+
   geom_smooth(method= "lm", color= "black", se=F)+
-  facet_wrap(~trait, scales= "free")+
   ggthemes::theme_tufte(base_size=12) + 
-  labs(tag = "A)", x= "Growing Degree Days (GDD)")+
-  geom_text (data= sig.label, aes(x= x, y= y, label = label), size= 4.5)+
+  labs(tag = "A)", x= "Growing Degree Days (GDD)", y= "Oil content (%)")+
+  annotate("text", label="Post. mean: - 0.0002\n CI: [ - 0.0006 | 0.0002 ]", x=2000, y=32, size=3)+
   theme(text = element_text(family = "sans"),
-        #plot.title= element_text(hjust = 0.5, size= 14, face = "bold"),
-        strip.text = element_text(size= 12, face = "bold"),
         plot.tag.position =c(0.01,0.99), 
         #plot.margin = unit(c(0,0,0,0),'cm'),
         legend.position = "none",  
-        legend.title = element_text(size = 12, color = "black", face="bold"),
-        legend.text = element_text(size = 10, color = "black"),
-        legend.margin = margin(0, 0, 0, 0),
         panel.background = element_rect(color = "black", fill = NULL),
-        axis.title.y = element_blank(),
-        axis.title.x = element_text(12),
-        axis.text.x = element_text(12),
-        axis.text.y = element_text(size = 10, color = "black")) -> fig5A;fig5A 
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black")) -> fig5A_oil;fig5A_oil
+
+# ratio
+read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
+  mutate(family = as.factor(family))%>%
+  mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
+                              "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
+                              "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
+                              "Crassulaceae", "Saxifragaceae", "Poaceae", 
+                              "Cyperaceae", "Juncaceae"))%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
+  na.omit()%>%
+  ggplot(aes(y=ratio, x = GDD))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = F, size= 4)+
+  scale_fill_manual (values=col_order)+
+  geom_smooth(method= "lm", color= "black", se=F)+
+  ggthemes::theme_tufte(base_size=12) + 
+  labs(x= "Growing Degree Days (GDD)", y= "UFA/SFA ratio")+
+  annotate("text", label="Post. mean: - 0.00006\n CI: [ - 0.0002 | 0.0002 ]", x=2000, y=18.8, size=3)+
+  theme(text = element_text(family = "sans"),
+        #plot.margin = unit(c(0,0,0,0),'cm'),
+        legend.position = "none",  
+        panel.background = element_rect(color = "black", fill = NULL),
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black")) -> fig5A_ratio;fig5A_ratio
+
+# combine
+fig5A_oil + fig5A_ratio + 
+  plot_layout(guides = 'auto', axis_titles = "collect_x")-> fig5A;fig5A
 
 ## PANEL b) OIL content and ratio vs FDD ####
-
-sig.label2 <- as.data.frame(cbind(trait = c("Oil content (%)", "Ratio UFA/SFA"),
-                                 x= c(150, 150), y= c(30, 18), 
-                                 label = c("pMCMC: 0.5", "pMCMC: 0.68")))
-sig.label2%>%
-  mutate(x= as.numeric(x),
-         y= as.numeric(y))->sig.label2
-str(sig.label2)
-
+# oil content
 read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
   mutate(family = as.factor(family))%>%
   mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
                               "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
                               "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
                               "Crassulaceae", "Saxifragaceae", "Poaceae", 
                               "Cyperaceae", "Juncaceae"))%>%
-  dplyr::select(community, Taxon, family, FDD, oil.content, ratio)%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
   na.omit()%>%
-  group_by(community, Taxon, family, FDD)%>%
-  gather(trait, value, oil.content:ratio)%>%
-  mutate(trait = as.factor(trait))%>%
-  mutate(trait = recode(trait, "oil.content"= "Oil content (%)", "ratio"= "Ratio UFA/SFA"))%>%
-  #data.frame()
-  ggplot(aes(y=value, x = FDD))+ #, fill = family
-  geom_point(aes(fill = family),shape = 21, color = "black", show.legend = F, size= 4)+
-  scale_fill_manual (values=col)+
+  ggplot(aes(y=oil.content, x = FDD))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = F, size= 4)+
+  scale_fill_manual (values=col_order)+
   geom_smooth(method= "lm", color= "black", se=F)+
-  facet_wrap(~trait, scales= "free")+
   ggthemes::theme_tufte(base_size=12) + 
-  labs(tag = "B)", x = "Freezing Degree Days (FDD)")+
-  geom_text (data= sig.label2, aes(x= x, y= y, label = label), size= 4.5)+
+  labs(tag = "B)", x= "Freezing Degree Days (FDD)", y= "Oil content (%)")+
+  annotate("text", label="Post. mean: 0.002\n CI: [ - 0.003 | 0.008 ]", x=140, y=30, size=3)+
   theme(text = element_text(family = "sans"),
-        #plot.title= element_text(hjust = 0.5, size= 14, face = "bold"),
-        strip.text = element_text(size= 12, face= "bold" ),
         plot.tag.position =c(0.01,0.99), 
         #plot.margin = unit(c(0,0,0,0),'cm'),
         legend.position = "none",  
-        legend.title = element_text(size = 12, color = "black", face="bold"),
-        legend.text = element_text(size = 10, color = "black"),
-        legend.margin = margin(0, 0, 0, 0),
         panel.background = element_rect(color = "black", fill = NULL),
-        axis.title.y = element_blank(),
-        axis.title.x = element_text(12),
-        axis.text.x = element_text(12),
-        axis.text.y = element_text(size = 10, color = "black"))-> fig5B;fig5B
-## PANEL c) OIL content and ratio vs Snow ####
-sig.label3 <- as.data.frame(cbind(trait = c("Oil content (%)", "Ratio UFA/SFA"),
-                                  x= c(140, 140), y= c(30, 18), 
-                                  label = c("pMCMC: 0.17", "pMCMC: 0.09")))
-sig.label3%>%
-  mutate(x= as.numeric(x),
-         y= as.numeric(y))->sig.label3
-str(sig.label3)
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black"))-> fig5B_oil;fig5B_oil
+
+# ratio 
 read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
   mutate(family = as.factor(family))%>%
   mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
                               "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
                               "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
                               "Crassulaceae", "Saxifragaceae", "Poaceae", 
                               "Cyperaceae", "Juncaceae"))%>%
-  dplyr::select(community, Taxon, family, Snw, oil.content, ratio)%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
   na.omit()%>%
-  group_by(community, Taxon, family, Snw)%>%
-  gather(trait, value, oil.content:ratio)%>%
-  mutate(trait = as.factor(trait))%>%
-  mutate(trait = recode(trait, "oil.content"= "Oil content (%)", "ratio"= "Ratio UFA/SFA"))%>%
-  #data.frame()
-  ggplot(aes(y=value, x = Snw))+ #, fill = family
-  geom_point(aes(fill = family),shape = 21, color = "black", show.legend = T, size= 4)+
-  scale_fill_manual (values=col, guide=guide_legend(nrow = 3))+
+  ggplot(aes(y=ratio, x = FDD))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = F, size= 4)+
+  scale_fill_manual (values=col_order)+
   geom_smooth(method= "lm", color= "black", se=F)+
-  facet_wrap(~trait, scales= "free")+
   ggthemes::theme_tufte(base_size=12) + 
-  labs(tag = "C)", x= "Snow days")+
-  geom_text (data= sig.label3, aes(x= x, y= y, label = label), size= 4.5)+
+  labs( x= "Freezing Degree Days (FDD)", y= "UFA/SFA ratio")+
+  annotate("text", label="Post. mean: 0.0008\n CI: [ - 0.002 | 0.004 ]", x=140, y=18, size=3)+
   theme(text = element_text(family = "sans"),
-        #plot.title= element_text(hjust = 0.5, size= 14, face = "bold"),
-        strip.text = element_text(size= 12, face= "bold"),
+        #plot.margin = unit(c(0,0,0,0),'cm'),
+        legend.position = "none",  
+        panel.background = element_rect(color = "black", fill = NULL),
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black"))-> fig5B_ratio;fig5B_ratio
+
+# combine
+fig5B_oil + fig5B_ratio + 
+  plot_layout(guides = 'auto', axis_titles = "collect_x")-> fig5B;fig5B
+## PANEL c) OIL content and ratio vs Snow ####
+# oil content
+read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
+  mutate(family = as.factor(family))%>%
+  mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
+                              "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
+                              "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
+                              "Crassulaceae", "Saxifragaceae", "Poaceae", 
+                              "Cyperaceae", "Juncaceae"))%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
+  na.omit()%>%
+  ggplot(aes(y=oil.content, x = Snw))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = T, size= 4)+
+  scale_fill_manual (values=col_order)+
+  geom_smooth(method= "lm", color= "black", se=F)+
+  ggthemes::theme_tufte(base_size=12) + 
+  labs(tag = "C)", x= "Snow cover (days)", y= "Oil content (%)")+
+  annotate("text", label="Post. mean: 0.003\n CI: [ - 0.001 | 0.007 ]", x=130, y=28, size=3)+
+  theme(text = element_text(family = "sans"),
         plot.tag.position =c(0.01,0.99), 
         #plot.margin = unit(c(0,0,0,0),'cm'),
-        legend.position = "bottom",  
-        legend.title = element_blank(),
-        legend.text = element_text(size = 10, color = "black"),
-        legend.margin = margin(0, 0, 0, 0),
+        legend.position = "none", 
         panel.background = element_rect(color = "black", fill = NULL),
-        axis.title.y = element_blank(),
-        axis.title.x = element_text(12),
-        axis.text.x = element_text(12),
-        axis.text.y = element_text(size = 10, color = "black"))-> fig5C;fig5C
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black"))-> fig5C_oil;fig5C_oil
 
-# combine plots ##
+# ratio
+read.csv("data/species_traits.csv")%>%
+  merge(read.csv("data/species_header.csv"), by = c ("Taxon", "community"))%>%
+  mutate(family = as.factor(family))%>%
+  mutate(family = fct_relevel(family,"Asteraceae","Campanulaceae",  "Apiaceae","Lamiaceae", "Orobanchaceae" ,
+                              "Plantaginaceae","Gentianaceae" ,"Primulaceae","Caryophyllaceae", "Plumbaginaceae",  
+                              "Fabaceae","Salicaceae","Brassicaceae", "Cistaceae", 
+                              "Crassulaceae", "Saxifragaceae", "Poaceae", 
+                              "Cyperaceae", "Juncaceae"))%>%
+  mutate(order = as.factor(order))%>%
+  mutate(order = fct_relevel(order,"Asterales", "Apiales","Lamiales",
+                             "Gentianales" ,"Ericales","Caryophyllales",  
+                             "Fabales","Malpighiales","Brassicales", "Malvales", 
+                             "Saxifragales", "Poales"))%>% 
+  dplyr::select(community, Taxon, order, family, GDD, FDD, Snw, oil.content, ratio)%>%
+  na.omit()%>%
+  ggplot(aes(y=ratio, x = Snw))+ #, fill = family
+  geom_point(aes(fill = order),shape = 21, color = "black", show.legend = T, size= 4)+
+  scale_fill_manual (values=col_order)+
+  geom_smooth(method= "lm", color= "black", se=F)+
+  ggthemes::theme_tufte(base_size=12) + 
+  labs( x= "Snow cover (days)", y= "UFA/SFA ratio")+
+  annotate("text", label="Post. mean: 0.002\n CI: [ - 0.0003 | 0.004 ]", x=130, y=16.5, size=3)+
+  theme(text = element_text(family = "sans"),
+        #plot.margin = unit(c(0,0,0,0),'cm'),
+        legend.position = "none", 
+        panel.background = element_rect(color = "black", fill = NULL),
+        axis.title = element_text(9),
+        axis.text = element_text(size = 9, color = "black"))-> fig5C_ratio;fig5C_ratio
 
-library(patchwork)
-fig5A/fig5B/fig5C + plot_layout(heights = c(1, 1, 1), guides = 'collect')&theme(legend.position = 'bottom')-> fig5;fig5
+# combine
+
+(fig5C_oil + fig5C_ratio) + 
+  
+  plot_layout(guides = 'collect', axis_titles = "collect_x")& 
+  theme(legend.position = 'bottom', legend.title = element_blank())-> fig5C;fig5C
+
+# FINAL combine plots ##
+fig5A/fig5B/fig5C + 
+  plot_annotation(title= "Ecological drivers")+
+  plot_layout(heights = c(1, 1, 1), guides = 'collect')&
+  theme(plot.title = element_text(size=14, hjust = 0.5))-> fig5;fig5
 
 # save plot
 ggsave(filename = "fig 5 ecological tradeoffs.png", plot =fig5 , path = "results/figures", 
-       device = "png", dpi = 600)
+       device = "png", dpi = 600, width = 180, units = "mm")
